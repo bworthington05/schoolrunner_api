7c07b569d8f6a862ca96f6190edc703d60c54c23
bworthington <bworthington05@gmail.com>
2016-02-07 22:35:43 +0000

    more work on attendance graph

--- network_data_digest/AttendanceComparison.java
+++ network_data_digest/AttendanceComparison.java
@@ -1,6 +1,4 @@
 import java.sql.*;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -11,6 +9,15 @@ import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.ChartColor;
+import org.jfree.chart.renderer.category.StackedBarRenderer;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.axis.NumberAxis;
+import java.text.NumberFormat;
+import org.jfree.chart.axis.CategoryAxis;
+import org.jfree.chart.axis.CategoryLabelPositions;
+import org.jfree.chart.renderer.category.StandardBarPainter;
+import java.awt.Color;
 
 //summarizes attendance data for network data digest, generates a .csv and jpeg bar graph
 public class AttendanceComparison {
@@ -53,7 +60,7 @@ public class AttendanceComparison {
     
     //file paths for files to be saved
     String csvFileName = "/home/ubuntu/workspace/output/data_digest/Attendance_Comparison_" + today + ".csv";
-    String graphFileName = "/home/ubuntu/workspace/output/data_digest/Attendance_Comparison_Graph_" + today + ".jpeg";
+    String chartFileNameString = "/home/ubuntu/workspace/output/data_digest/Attendance_Comparison_Graph_" + today + ".jpeg";
     
     //custom Absences endpoint url that includes the parameters for active=1, min_date = minDate, and max_date = today
     String absencesEndpoint = "https://renew.schoolrunner.org/api/v1/absences/?limit=30000&active=1&min_date=" + this.minDate +
@@ -82,20 +89,19 @@ public class AttendanceComparison {
         "SELECT " +
             "schools.display_name, " +
             
-            //for each school, divide the # of attendance records by the # of active students and multiply by 1000
-            //round the result to 0 decimal places
+            //for each school, get the number of of specified attendance codes recorded
             
-            "ROUND(((CAST((SUM(CASE WHEN absence_types.absence_code = 'A' THEN 1 ELSE 0 END)) AS FLOAT) " + 
-              "/ active_students.count_of_students) * 100), 0), " +
+            "SUM(CASE WHEN absence_types.absence_code = 'A' THEN 1 ELSE 0 END), " +
               
-            "ROUND(((CAST((SUM(CASE WHEN absence_types.absence_code = 'AE' THEN 1 ELSE 0 END)) AS FLOAT) " + 
-              "/ active_students.count_of_students) * 100), 0), " +
+            "SUM(CASE WHEN absence_types.absence_code = 'AE' THEN 1 ELSE 0 END), " +
               
-            "ROUND(((CAST((SUM(CASE WHEN absence_types.absence_code = 'T' THEN 1 ELSE 0 END)) AS FLOAT) " + 
-              "/ active_students.count_of_students) * 100), 0), " +
+            "SUM(CASE WHEN absence_types.absence_code = 'T' THEN 1 ELSE 0 END), " +
               
-            "ROUND(((CAST((SUM(CASE WHEN absence_types.absence_code = 'TE' THEN 1 ELSE 0 END)) AS FLOAT) " + 
-              "/ active_students.count_of_students) * 100), 0) " +
+            "SUM(CASE WHEN absence_types.absence_code = 'TE' THEN 1 ELSE 0 END), " +
+            
+            //subtract sum of tardies & absences from (active student count * # of school days in this range- i.e., "student days") 
+            //to get the value for the top (filler) part of barchart
+            "(active_students.count_of_students*4)-(SUM(CASE WHEN absence_types.absence_code IN ('A','AE','T','TE') THEN 1 ELSE 0 END))" +
             
         "FROM absences " +
             "LEFT OUTER JOIN absence_types ON absences.absence_type_id = absence_types.absence_type_id " +
@@ -113,7 +119,7 @@ public class AttendanceComparison {
 
         "WHERE absences.active = '1' AND absences.sr_school_id NOT IN ('5','17','18') " +
         "GROUP BY schools.display_name " +
-        "ORDER BY schools.display_name; ");
+        "ORDER BY schools.ps_school_id, schools.display_name; ");
 
       stmt = c.createStatement();
       ResultSet rs = stmt.executeQuery(query);
@@ -134,6 +140,8 @@ public class AttendanceComparison {
 	    writer.append("# of Tardy");
 	    writer.append(',');
 	    writer.append("# of Tardy Excused");
+	    writer.append(',');
+	    writer.append("Everyone Else");
 	    writer.append('\n');
       
       //loop throw the SQL resultset and save records to the filewriter and jfreechart dataset
@@ -150,6 +158,8 @@ public class AttendanceComparison {
   	    writer.append("\"" + rs.getString(4) + "\"");
   	    writer.append(',');
   	    writer.append("\"" + rs.getString(5) + "\"");
+  	    writer.append(',');
+  	    writer.append("\"" + rs.getString(6) + "\"");
   	    writer.append('\n');
     	    
         //convert the string values to doubles
@@ -157,28 +167,78 @@ public class AttendanceComparison {
         double double3 = Double.parseDouble(rs.getString(3));
         double double4 = Double.parseDouble(rs.getString(4));
         double double5 = Double.parseDouble(rs.getString(5));
+        double double6 = Double.parseDouble(rs.getString(6));
         
         //add each row of the resultset to the dataset (value, school name, attendance type)
-        dataset.addValue(double2, rs.getString(1), "Absent");
-        dataset.addValue(double3, rs.getString(1), "Absent Excused");
-        dataset.addValue(double4, rs.getString(1), "Tardy");
-        dataset.addValue(double5, rs.getString(1), "Tardy Excused");
+        dataset.addValue(double2, "Absent", rs.getString(1));
+        dataset.addValue(double3, "Absent Excused", rs.getString(1));
+        dataset.addValue(double4, "Tardy", rs.getString(1));
+        dataset.addValue(double5, "Tardy Excused", rs.getString(1));
+        
+        //last value is student days - tardies & absences
+        //which is the remaining top part of the stacked barchart (everyone else who wasnt absent or tardy)
+        dataset.addValue(double6, "Everyone Else", rs.getString(1));
       }
       
-      JFreeChart barChart = ChartFactory.createBarChart(
+      //make the stacked barchart
+      JFreeChart chart = ChartFactory.createStackedBarChart(
         "Attendance Comparison: " + prettyMinDate + " - " + prettyToday, //graph title
         "Small School", //legend label
-        "# per 100 Students", //vertical axis label 
+        "Avg. % of Students per Day", //vertical axis label 
         dataset, //dataset being used
         PlotOrientation.VERTICAL, //bar orientation
         true, //include legend
         true, //generate tooltips
         false); //generate URLs
+      
+      //absent color = red
+      ChartColor absentColor = new ChartColor(252, 28, 3);
+      
+      //absent excused color = orange
+      ChartColor absentExcusedColor = new ChartColor(253, 165, 2);
+      
+      //tardy color = blue
+      ChartColor tardyColor = new ChartColor(40, 85, 219);
+      
+      //tardy excused color = purplish
+      ChartColor tardyExcusedColor = new ChartColor(188, 101, 209);
+      
+      //everyone else color = greenish
+      ChartColor everyoneElseColor = new ChartColor(113, 196, 128);
+      
+      //create renderer to customize the chart
+      CategoryPlot plot = chart.getCategoryPlot();
+      StackedBarRenderer renderer = (StackedBarRenderer) plot.getRenderer();
+      renderer.setBarPainter(new StandardBarPainter());
+      
+      //make the dashed lines that go across the chart black
+      plot.setRangeGridlinePaint(Color.BLACK);
+      
+      renderer.setSeriesPaint(0, absentColor);
+      renderer.setSeriesPaint(1, absentExcusedColor);
+      renderer.setSeriesPaint(2, tardyColor);
+      renderer.setSeriesPaint(3, tardyExcusedColor);
+      renderer.setSeriesPaint(4, everyoneElseColor);
+      
+      //renders each section of a bar as a percent out of 100
+      renderer.setRenderAsPercentages(true);
+      
+      NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
+      
+      //set the range to be between 0 and 1.0 (or 0% and 100%)
+      rangeAxis.setRange(0.0, 1.0);
+      
+      //set the range axis (Y axis) scale to show percents as whole numbers
+      rangeAxis.setNumberFormatOverride(NumberFormat.getPercentInstance());
+      
+      //set the category labels on the X axis to be written vertically
+      CategoryAxis categoryAxis = (CategoryAxis) plot.getDomainAxis();
+      categoryAxis.setCategoryLabelPositions(CategoryLabelPositions.UP_90);
          
       int width = 768; //width of the image
       int height = 576; //height of the image
-      File graphFileNameFile = new File(graphFileName);
-      ChartUtilities.saveChartAsJPEG(graphFileNameFile, barChart, width, height);
+      File chartFileName = new File(chartFileNameString);
+      ChartUtilities.saveChartAsJPEG(chartFileName, chart, width, height);
       
       rs.close();
       stmt.close();
